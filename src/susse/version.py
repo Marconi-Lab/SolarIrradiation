c9c70e4a1484adb5b49ba32298529e52f68fd337
khristian7 <christiannyakairu@gmail.com>
2024-09-13 13:58:34 +0300

    Updated

--- src/susse/version.py
+++ src/susse/version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev35+gd792bda.d20240913'
-__version_tuple__ = version_tuple = (0, 1, 'dev35', 'gd792bda.d20240913')
+__version__ = version = '0.1.dev38+g2326dc9.d20240913'
+__version_tuple__ = version_tuple = (0, 1, 'dev38', 'g2326dc9.d20240913')
