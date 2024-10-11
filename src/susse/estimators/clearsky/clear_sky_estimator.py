1e3d3521a9f203f44c8329525f4d2b345c93f8c2
Jan Mikelson <jan.mikelson@gmail.com>
2024-10-11 17:58:13 +0200

    estimators refactored

--- src/susse/estimators/clearsky/clear_sky_estimator.py
+++ src/susse/estimators/clearsky/clear_sky_estimator.py
@@ -3,29 +3,45 @@ from abc import ABC, abstractmethod
 from geopy import location as Glocation
 import datetime
 
+from .clear_sky_data import ClearSkyEstimate
+
 import pandas as pd
 
 from overrides import override
 
 import pvlib
 
+from enum import Enum
+
 
 class ClearSkyEstimator(ABC):
+    """
+    This class is the baseclass for all estimators of Clear-Sky irradiance. it takes as inputs the location of interest
+    as well as the start and end date. The return value is of type ClearSkyEstimate, which itself is a wrapper for a pandas dataframe
+    """
 
     def __init__(self, name: str):
         self._name = name
         self._timedelta = datetime.timedelta(hours=1)
-        self.
+        self._tz = datetime.timezone.utc
 
     @abstractmethod
-    def estimate_clear_sky(self, location: Glocation, start_date: datetime.datetime, end_date: datetime.datetime):
+    def estimate_clear_sky(self, location: Glocation, start_date: datetime.datetime, end_date: datetime.datetime) -> ClearSkyEstimate:
         pass
 
     def set_timedelta(self, timedelta: datetime.timedelta):
         self._timedelta = timedelta
 
+    def set_timezone(self, tz: datetime.timezone):
+        self._tz = tz
+
 
 class ClearSkyEstimatorPVlib(ClearSkyEstimator):
+
+    class Models(Enum):
+        SOLIS = "simplified_solis"
+        INEICHEN = "ineichen"
+        HAURWITZ = "haurwitz"
 
     def __init__(self):
         super().__init__("clear_sky_pvlib")
@@ -36,7 +52,8 @@ class ClearSkyEstimatorPVlib(ClearSkyEstimator):
         pv_location = pvlib.location.Location(location.latitude, location.longitude, altitude=location_altitude)
 
         times = pd.date_range(
-            start=start_date, end=end_date, freq=self._timedelta, tz=self.location.tz
+            start=start_date, end=end_date, freq=self._timedelta, tz=self._tz
         )
-        clear_sky_df = pv_location.get_clearsky()
+        clear_sky_df = pv_location.get_clearsky(times=times, model=self.Models.SOLIS.value)
+        return ClearSkyEstimate(clear_sky_df)
 
