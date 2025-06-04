ec01e3a10d476ede7078e0ec9bd2c27c0fb340b8
Jan Mikelson <jan.mikelson@gmail.com>
2025-06-04 09:55:05 +0200

    minor renaming

--- tests/api_clients/test_nasa_power.py
+++ tests/api_clients/test_nasa_power.py
@@ -9,7 +9,7 @@ from susse.api_clients import (
     NASAPowerConfig,
     NASAPowerDataResult,
     NASAPowerFetchData,
-    NASAPowerProducts,
+    NASAPowerProduct,
     TemporalResolution,
 )
 
@@ -46,7 +46,7 @@ def test_url_generation(resolution, code, dates, location):
         start,
         end,
         location,
-        NASAPowerProducts.SURFACE_PRESSURE,
+        NASAPowerProduct.SURFACE_PRESSURE,
     )
 
     assert f"/temporal/{code}/point" in url
@@ -58,7 +58,7 @@ def test_url_generation(resolution, code, dates, location):
 
 def test_fetch_data_success(requests_mock, fetcher, dates, location):
     start, end = dates
-    product = NASAPowerProducts.SURFACE_PRESSURE
+    product = NASAPowerProduct.SURFACE_PRESSURE
 
     # Prepare mock URL and JSON response
     url = NASAPowerConfig.generate_download_link(
@@ -96,7 +96,7 @@ def test_fetch_data_success(requests_mock, fetcher, dates, location):
 
 def test_fetch_data_http_error(requests_mock, fetcher, dates, location):
     start, end = dates
-    product = NASAPowerProducts.SURFACE_PRESSURE
+    product = NASAPowerProduct.SURFACE_PRESSURE
 
     url = NASAPowerConfig.generate_download_link(
         TemporalResolution.HOURLY,
@@ -127,7 +127,7 @@ def sample_result(dates, location) -> NASAPowerDataResult:
     }
     return NASAPowerDataResult(
         data=data,
-        product=NASAPowerProducts.SURFACE_PRESSURE,
+        product=NASAPowerProduct.SURFACE_PRESSURE,
         location=location,
         start_date=start,
         end_date=end,
@@ -151,7 +151,7 @@ def test_timestamp_ordering(location, dates):
     }
     result = NASAPowerDataResult(
         data=unsorted,
-        product=NASAPowerProducts.SURFACE_PRESSURE,
+        product=NASAPowerProduct.SURFACE_PRESSURE,
         location=location,
         start_date=start,
         end_date=end,
